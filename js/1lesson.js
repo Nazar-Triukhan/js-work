a2435ab0d8f5331b201b5636901f0d7eb2534231
Nazar <tryuhann@gmail.com>
2025-09-04 10:04:11 +0300

    фвв

--- js/1lesson.js
+++ js/1lesson.js
@@ -1,12 +1,12 @@
-const name = 'nazik';
-console.log(name);  
-let lastName = 'triukhan';
-console.log(lastName);
-console.log(typeof lastName);
-const namber = 16;
-console.log(namber);
-console.log(typeof number);
-const number2 = 5>2;
-console.log(number2);
+// const name = 'nazik';
+// console.log(name);  
+// let lastName = 'triukhan';
+// console.log(lastName);
+// console.log(typeof lastName);
+// const namber = 16;
+// console.log(namber);
+// console.log(typeof number);
+// const number2 = 5>2;
+// console.log(number2);
 
 
---
+++ js/2lesson.js
@@ -0,0 +1,22 @@
+// const messege = 'HEllo,My namE is Nazar';
+// console.log(messege.length);
+// console.log(typeof(messege));
+// const upperMessege = messege.toLowerCase()
+// const upperMessege = messege.toLocaleUpperCase()
+// console.log(upperMessege);
+
+// alert(upperMessege)
+// const result = confirm(messege)
+// console.log(result);
+// const userName = prompt('Ведіть ваше імя:')
+// console.log(userName);
+
+// const ageNazar = 15;
+// const ageVlad = 16;
+
+// const result = ageNazar + ageVlad;
+// console.log(result);
+
+const from = prompt('Where are you from:');
+const sity = prompt('Where are you sity:');
+console.log('Ви проживаєте в місці:', sity, 'ви знаходитесь:', from);
