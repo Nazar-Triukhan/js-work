e37b3b7309c71c7e781d9fa6e7ef1f122c3264d2
Nazar <tryuhann@gmail.com>
2025-09-09 23:15:11 +0300

    add

---
+++ home-work_arhiv/strock.js
@@ -0,0 +1,31 @@
+const age = 16;
+console.log(age);
+
+const name = 'Nazik';
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = 'Немає улюбленої цитати';
+console.log(myString);
+
+let myNumber = 10;
+myNumber = myNumber +10;
+console.log(myNumber);
+
+const  myNul = null;
+console.log( myNul);
+
+const userName = prompt('Ведіть своє імя');
+console.log(userName);
+alert(`Привіт ${userName}`)
+console.log(typeof userName);
+
+const userAnswer = confirm('Ви дійсно хочете покинути сторінку?')
+console.log(userAnswer);
+
+const userCareles = 'Увага! Дія може бути небезпечною.'
+alert(userCareles)
+const userOkey = confirm('Ви впевнені, що хочете продовжити?')
+console.log(userOkey);
