48c0e874926d4bcaee690e5e04e1a9560be7e15c
Nazar <tryuhann@gmail.com>
2025-09-11 09:36:12 +0300

    add

---
+++ числа/numbers2_math.js
@@ -0,0 +1,33 @@
+const max = Math.max(22,55,12,54,13,75,23,6);
+console.log(max);
+
+const min = Math.min(2,45,5,2,5,6,4,3,-5,33);
+console.log(min);
+
+const step = Math.pow(5 , 2)
+console.log(step);
+
+const bow = Math.floor(12.34)
+console.log(bow);
+
+const cale = Math.ceil(12.34)
+console.log(cale);
+
+const round = Math.round(12.34)
+console.log(round);
+
+const skrt = Math.sqrt(25)
+console.log(skrt);
+
+const random = Math.random() * (100 - 0) 
+console.log(Math.floor(random));
+
+// Напишіть скрипт, який знаходить площу прямокутника. Висота 23см, шириною 10см - 
+// всі данні повинні зберігатися в змінних.  
+// Значення площі повинно зберігається в числовій змінній.
+const heit = '23sm';
+const width = '10sm';
+const heitInt = parseInt(heit)
+const idthInt = parseInt(width)
+const result = heitInt * idthInt
+console.log(result);
