cf973a5a6c9b5359c76081d93dc3250b9c60141e
Nazar <tryuhann@gmail.com>
2025-12-03 15:45:01 +0200

    add

---
+++ Імперативний-декларативний/lesson1.js
@@ -0,0 +1,46 @@
+// const arr = [1,2,3,4,5,6,7,8,9]
+// const test = arr.forEach((item,index,arrr)=>{console.log(arrr);})
+
+// масив.метод(()=>{})
+    //масив.метод((item, idx, arr)=> item)
+//     const dataArray = [
+//   {
+//     id: 101,
+//     name: "Олег",
+//     isActive: true,
+//   },
+//   {
+//     id: 102,
+//     name: "Марія",
+//     isActive: false,
+//   },
+//   {
+//     id: 103,
+//     name: "Віктор",
+//     isActive: true,
+//   },
+//   {
+//     id: 104,
+//     name: "Тетяна",
+//     isActive: true,
+//   },
+//   {
+//     id: 105,
+//     name: "Андрій",
+//     isActive: false,
+//   },
+// ];
+// for(let i = 0;i < dataArray.length;i++){
+//     console.log(dataArray[i]);
+// }
+// dataArray.forEach((item)=>console.log(item))
+// нічого не повертає і може дефікувати
+
+
+
+// метод Map: переберає,повертає новий масив,де до кожного елемента щось застосує
+// const arr = [1,2,3,4,5,6,7,8,9]
+// const arrMap = arr.map((item)=>item * 10)
+// console.log(arrMap);
+
+
